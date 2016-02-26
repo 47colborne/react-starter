9bd187b94e0c916812e319f8566894c9ef237840
scottchu <scottchu@icloud.com>
2016-02-25 23:41:08 -0500

    initial commit

---
+++ app/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import '../styles/app';
+
+class App extends Component {
+	render () {
+		return (
+			<div id="app">
+
+			</div>
+		)
+	}
+}
+
+export default App;
